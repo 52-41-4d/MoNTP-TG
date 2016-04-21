7ca6139c97ca30b9ca1af66eca6120b56308f8f8
Ramakrishnan Durairajan <ramakrish007@gmail.com>
2016-04-20 19:25:58 -0500

    Added .gitignore

--- runComparisonMeasurements.sh
+++ runComparisonMeasurements.sh
@@ -1,6 +1,6 @@
 # sudo python realTimeWiFiMonitor.py & python realTimeNTPMonitor.py
 
-name="run1_7385-6"
+name="run3_7385-6_normalConnection"
 sudo python realTimeWiFiMonitor.py > wifiOutput_${name}.txt & python realTimeNTPMonitor.py > ntpOutput_${name}.txt
 join -t , -1 1 -2 1 -a 1 wifiOutput_${name}.txt ntpOutput_${name}.txt > runOutput_${name}.txt
 python plotOutput.py ${name}
