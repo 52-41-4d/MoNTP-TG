6d6f32eb34510c5834c2248a86f8b880971de5db
Ramakrishnan Durairajan <ramakrish007@gmail.com>
2016-04-24 16:16:49 -0500

    fixed target send END

--- ClientServer/targetClient.py
+++ ClientServer/targetClient.py
@@ -35,4 +35,4 @@ if __name__ == "__main__":
         sendPingData(commandServer, commandServerPort, rtt)
         time.sleep(1)
 
-    sendPingData("END")
+    sendPingData(commandServer, commandServerPort, "END")
